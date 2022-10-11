d64dcb81e939f2717f4eec61f4ed40f8f9ffdd9b
JamesR404v2 <james.kuijpers@web.de>
2022-10-11 09:38:15 +0200

    added comments
    Just added comments to describe the FRS, at 85% of reading the FRS

---
+++ src/main/java/com/coderscampus/myapp/main/HigherLowerGuessingGame.java
@@ -0,0 +1,46 @@
+package com.coderscampus.myapp.main;
+
+import com.coderscampus.myapp.service.MyService;
+
+public class HigherLowerGuessingGame {
+	
+	private MyService myService = new MyService();
+	
+	public static void main(String[] args) {
+		new HigherLowerGuessingGame().execute();
+	}
+
+	private void execute() {
+		// Your code goes here, not in the main() method
+		// This removes any need to consume statics
+
+		
+		// Generate random int number between 1 and 100 inclusive.
+		
+		// set guessAttemptsRemaining to 5
+		
+		
+		// User guesses. "Pick a number between 1 and 100"
+		// validate userInput and ensure it is a number between 1 and 100, if not, ask for input again.
+		// when valid, check how the userInput matches the randomNumber and return output
+//		1.       “Please pick a higher number” (if the number they chose is lower than the randomly generated number), or
+//		then substract a guessAttemptsRemaining, if still more than 0 guesses remaining, give another guess
+//		  
+//
+//		2.       “Please pick a lower number” (if the number they chose is higher than the randomly generated number), or
+//		then substract a guessAttemptsRemaining, if still more than 0 guesses remaining, give another guess
+//		  
+//
+//		3.       “You win!” (if they guess the correct number)
+		// exit programm.
+//		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
