6bd9933a1c1b485c4822b8e292df15cdb1d9ced0
JamesR404v2 <james.kuijpers@web.de>
2022-10-15 17:50:05 +0200

    Fixed String updating & moving code for Integer conversion to Recursive Function
    Fixed the issue of not being able to update the string, by simply declaring the string again instead of trying to update it (which is not allowed) https://stackoverflow.com/questions/11924689/change-string-in-java
    https://stackoverflow.com/questions/11164675/mutable-strings-in-java
    
    Moved the code for converting the user input to an integer to its own method, so I can refer to itself - changing it into a recursive function.
    
    Got a new issue with the String not being convertable to a variable. Think it's an easy fix but I am out of time right now. Will resume later.

--- src/main/java/com/coderscampus/myapp/main/HigherLowerGuessingGame.java
+++ src/main/java/com/coderscampus/myapp/main/HigherLowerGuessingGame.java
@@ -14,7 +14,7 @@ public class HigherLowerGuessingGame {
 	
 	public static void main(String[] args) {
 
-//		String userInput = "no input yet";
+		String userInput = "no user input yet";
 		int userInputInt = 0;
 		
 		new HigherLowerGuessingGame().execute();
@@ -37,7 +37,7 @@ public class HigherLowerGuessingGame {
 		// User guesses. "Pick a number between 1 and 100"
 		
 		
-//		public String userInput = "user did not give any input yet";
+		String userInput = "user did not give any input yet";
 //		public String userInput;
 		
 		userInput = userGuess();
@@ -45,19 +45,10 @@ public class HigherLowerGuessingGame {
 		
 		// validate userInput and ensure it is a number between 1 and 100, if not, ask for input again.
 		
-//		int userInputInt = 0;
+		int userInputInt = 0;
 //		declaring this in the main function instead, so it is usable in the userGuess function.
 		
-//		first make it an integer, so it can be compared to other numbers.
-		try {
-			userInputInt = Integer.parseInt(userInput);			
-			}
-		catch(NumberFormatException nFE) {
-		    System.out.println("Not an Integer");
-			System.out.println("Your guess is not between 1 and 100, please try again");
-//			and go to the function to get new user input, without subtracting a remaining guess.
-			userInput = userGuess();
-			}
+userInputInt = convertUserInputToUserInputInt(userInput, userInputInt);
 		
 		
 		if ((userInputInt >= 1) == (userInputInt <= 100)) {
@@ -95,13 +86,31 @@ public class HigherLowerGuessingGame {
 		
 	}
 
+	public int convertUserInputToUserInputInt(String userInput, int userInputInt) {
+		//		first make it an integer, so it can be compared to other numbers.
+		try {
+//			int userInputInt = 0;
+			// declaring variable so it can be used.
+//			had it declared earlier in the script but still wasn't usable here....
+			userInputInt = Integer.parseInt(userInput);			
+			}
+		catch(NumberFormatException nFE) {
+		    System.out.println("Not an Integer");
+			System.out.println("Your guess is not between 1 and 100, please try again");
+//			and go to the function to get new user input, without subtracting a remaining guess.
+			userInput = userGuess();
+			convertUserInputToUserInputInt(String userInput, int userInputInt);
+			}
+		return userInputInt;
+	}
+
 	String userGuess() {
 		System.out.println("Pick a number between 1 and 100");
 		
 		java.util.Scanner scannerUserInput = new
 		java.util.Scanner(System.in);
 		
-		userInput = scannerUserInput.next();
+		String userInput = scannerUserInput.next();
 		return userInput;
 	}
 
